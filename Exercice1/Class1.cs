c62a1ab72245712708d9b8c821200cb2cb1e422a
AxelRigal <37696088+AxelRigal@users.noreply.github.com>
2019-10-16 23:04:59 +0200

    exercice 1

---
+++ Exercice1/Class1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Exercice1
+{
+    class Class1
+    {
+        public static char RetourneChaine(char c)
+        {
+            int test = (int)c;
+
+            if(test >= 120)
+            {
+                test = test + 3 - 26;
+            }
+            else
+            {
+                test = test + 3;
+            }
+            return Convert.ToChar(test);
+        }
+    }
+}
--- Exercice1/Program.cs
+++ Exercice1/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Text;
 
 namespace Exercice1
 {
@@ -7,6 +8,35 @@ namespace Exercice1
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine(12 + 5 * 12.5 - 1253.68);
+            Console.WriteLine("Moyenne de {1.0,5.5,9.9,2.8,9.6");
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine(" Rentre le rayon d'un Cercle ");
+            var rayon = Console.ReadLine();
+            StringBuilder stringBuilderPerimetre = new StringBuilder();
+            stringBuilderPerimetre.Append("Le périmetre du Cercle est: ");
+            double perimetre = (2 *  Convert.ToDouble(rayon)) * Math.PI ;
+            stringBuilderPerimetre.Append(perimetre);
+            Console.WriteLine(stringBuilderPerimetre);
+            StringBuilder stringbuilderSurface = new StringBuilder();
+            stringbuilderSurface.Append("La surface du Cercle est : ");
+            double surface = (Convert.ToDouble(rayon) * Convert.ToDouble(rayon)) * Math.PI;
+            stringbuilderSurface.Append(surface);
+            Console.WriteLine(stringbuilderSurface);
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine("Ecrire une chaine de 5 caractères");
+            string chaineString = Console.ReadLine();
+            char[] chaine = chaineString.ToCharArray();
+            chaine[0] = Class1.RetourneChaine(chaine[0]);
+            chaine[1] = Class1.RetourneChaine(chaine[1]);
+            chaine[2] = Class1.RetourneChaine(chaine[2]);
+            chaine[3] = Class1.RetourneChaine(chaine[3]);
+            chaine[4] = Class1.RetourneChaine(chaine[4]);
+            Console.WriteLine(chaine);
         }
     }
 }
